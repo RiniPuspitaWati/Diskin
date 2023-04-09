afdf6b8ba53cdce205df46590b0ff424f2c9b685
RiniPuspitaWati <112569358+RiniPuspitaWati@users.noreply.github.com>
2023-04-09 14:08:31 +0700

    Add files via upload

---
+++ Diskon.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+        int number;
+        cin>>number;
+        
+        if (number == 1){
+                cout<<"one";
+                
+        }        
+        if else (number == 2){
+                cout<<"two";
+        }
+        if else (number == 3){
+                cout<<"three";
+        }
+        if else (number == 4){
+                cout<<"four";
+        }
+        if else (number == 5){
+                cout<<"five";
+        }
+        if else (number == 6){
+                cout<<"six";
+        }
+        if else (number == 7){
+                cout<<"seven";
+        }
+        if else (number == 8){
+                cout<<"eight";
+        }
+        if else (number == 9){
+                cout<<"nine";
+        }
+        else{
+                cout<<"Greater than 9";
+        }
+        
+        return 0;
+}
